700f2e2710dde5782ca32554ce365182b68c3271
Richard Hriech <hardric501@gmail.com>
2018-07-25 14:27:22 +0200

    :)

---
+++ src/main/java/com/codecool/Tinker.java
@@ -0,0 +1,37 @@
+package com.codecool;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Tinker {
+
+    private List<Integer> tinkerList = new ArrayList<>();
+    private final int N;
+    private final int K;
+
+    public Tinker(int N, int K) {
+        this.N = N;
+        this.K = K;
+        for (int i = 1; i <= N; i++) {
+            tinkerList.add(i);
+        }
+    }
+
+    public void run() {
+        int iterator = K - 1;
+
+        while (!tinkerList.isEmpty()) {
+            iterator = iterator % tinkerList.size();
+            System.out.println(
+                tinkerList.remove(iterator--)
+            );
+            iterator += K;
+        }
+    }
+
+    public static void main(String[] args) {
+        Tinker tinker = new Tinker(5, 3);
+        tinker.run();
+    }
+}
